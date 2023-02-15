14da4d3af3228fe00785032af6b7c2156a2285e3
Valdecir Sarat <valdecir.sarat@gmail.com>
2023-02-15 18:24:02 -0300

    criando blog

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,15 @@
+export const data = [
+  {
+    "id": "1",
+    "photoCover":"https://blog.escoladocredito.com.br/wp-content/uploads/2022/11/placeholder-9.png",
+    "title":"Nova Versão do Angularlo",
+    "description":"Lorem ipsum"
+  },
+  {
+    "id": "2",
+    "photoCover":"https://www.mobileappdaily.com/public/uploads/mad_588212b2ce.png",
+    "title":"Angular novidades",
+    "description":"Lorem ipsum"
+  }
+]
+
